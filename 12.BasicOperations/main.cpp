0e009e3035e1ba2293fe260e9a941c4684eab905
Ndakhoa <nguyendinh.anhkhoa97@gmail.com>
2022-06-22 17:18:05 +0700

    Add part 12 Basic Operations

---
+++ 12.BasicOperations/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+	//Precedebce: which operartion to do first
+	//Associativity: which direction or which order
+
+	int a = 18;
+	int b = 7;
+
+	//Add
+	int result_ab = a + b;
+	cout << "a + b = " << result_ab << endl;
+
+	//Sub
+	result_ab = a - b; 
+	cout << "a - b = " << result_ab << endl;
+
+	result_ab = b - a;  
+	cout << "b - a = " << result_ab << endl;
+	
+	//Multiply
+	result_ab = a * b;
+	cout << "a * b = " << result_ab << endl;
+
+	//Division
+	result_ab = a / b;
+	double div_ab = a / b;
+	cout << "a / b = " << div_ab << endl;
+
+	//Modulus
+	result_ab = a % b;
+	cout << "a % b = " << result_ab << endl;
+	return 0;
+}
