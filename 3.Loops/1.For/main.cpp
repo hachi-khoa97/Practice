1df739d39382e968db51b7b0459e546dd9b9d173
Ndakhoa <nguyendinh.anhkhoa97@gmail.com>
2022-07-11 16:35:53 +0700

    Add For loop

---
+++ 3.Loops/1.For/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int i = 0;
+    // for(i; i < 10; i++){
+    //     cout << i << endl;
+    // }
+    // cout << "i outside: " << i << endl;
+
+    //Remark: Dont hardcode value of how many time loop run
+    //int count = 10
+    // for(i; i < count; i++){
+    //     cout << i << endl;
+    // }
+    // cout << "i outside: " << i << endl;
+    
+    //size_t: represent for unsigned data type
+    //Size of size_t based on platform
+    cout << "Size of size_t: " << sizeof(size_t) << endl;
+    return 0;
+}
+ 
